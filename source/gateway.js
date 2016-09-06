195a979e8092916daac7de2425d47d842d48d9e6
Faraz Syed <hello@farazsyed.com>
2016-09-05 19:09:27 -0600

    v0.7.0 add endpoint at '/api-documentation/basePath' to provide API spec

--- source/gateway.js
+++ source/gateway.js
@@ -4,6 +4,20 @@ const debug = require('debug')('centro:gateway');
 module.exports = function gateway(doc) {
   debug('entered plugin', doc.info.title);
 
+  const apiDocServiceName = 'gateway:api:' + doc.host + doc.basePath;
+
+  this.add(apiDocServiceName, (data, token, callback) => {
+    callback(null, {
+      data: doc
+    });
+  });
+
+  this.promisify('server:get', ('/api-documentation' + doc.basePath), apiDocServiceName)
+    .then(value => debug('success', value))
+    .catch(error => debug('fail', error));
+
+  debug('new endpoint', 'get', '\'/api-documentation ' + doc.basePath + '\'', '=>', apiDocServiceName);
+
   Object.keys(doc.paths)
     .forEach((path) => {
       Object.keys(doc.paths[path])
